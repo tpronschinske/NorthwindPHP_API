203c3cc358322441c02e0d24eb2fd8d4fa6acf92
Travis Pronschinske <tpronschinske@panatrack.com>
2019-10-24 17:04:20 -0500

    Added in new JWT and worked on setup for auth

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -16,19 +16,48 @@ class AuthController extends \Core\Controller {
     }
 
     public function login(){
-      try {
-        if(Request::isPost()){
-            $reqToken = Csrf::makeToken();
-            echo json_encode($reqToken);
-        }
-      } catch (Exception $e) {
-          http_response_code(400);
-          var_dump($e);
-          echo($e->getMessage());
-          echo($e->getCode());
-          exit;
-      }
+      // try {
+      //   if(Request::isPost()){
+      //       // $token = array (
+      //       //   "iss" => 'http://northwind.coonetcreative.net',
+      //       //   "aud" => "http://northwind.coonetcreative.net",
+      //       //   "iat" => 1356999524,
+      //       //   "nbf" => 1357000000
+      //       // );
+      //       // $jwt = JWT::encode($token, $key);
+      //       // $decoded = JWT::decode($jwt, $key, array('HS256'));
+      //       // echo json_encode($decoded);
+      //
+      //       // $reqToken = Csrf::makeToken();
+      //       // echo json_encode($reqToken);
+      //   }
+      //
+      //
+      // } catch (Exception $e) {
+      //     http_response_code(400);
+      //     var_dump($e);
+      //     echo($e->getMessage());
+      //     echo($e->getCode());
+      //     exit;
+      // }
 
+
+      $token = array (
+        "iss" => URL,
+        "aud" => URL,
+        "iat" => time(),
+        "nbf" => time() + 60
+      );
+      $jwt = JWT::encode($token, KEY);
+      JWT::$leeway = 60; // $leeway in seconds
+      $decoded = JWT::decode($jwt, KEY, array('HS256'));
+      $decoded_array = (array) $decoded;
+      $data['jwt'] = $jwt;
+      $data['decoded'] = $decoded_array;
+
+      View::renderTemplate('header');
+      View::render('auth/login', $data);
+      View::renderTemplate('footer');
 
     }
 
--- app/views/auth/login.php
+++ app/views/auth/login.php
@@ -4,25 +4,29 @@ use \Helpers\Form;
 use \Core\Error;
 
 ?>
-<style media="screen">
-    body{
-        background: transparent!important;
-    }
-</style>
+<div class="row">
+  <div class="col-sm-12">
+    <p><?php echo $data['jwt']; ?></p>
+    <p><?php var_dump($data['decoded']); ?></p>
+    <p><?php echo date('m/d/Y h:m:s', $data['decoded']['iat']) ?></p>
+    <p><?php echo date('m/d/Y h:m:s', $data['decoded']['nbf']) ?></p>
 
-<div class="login-wrapper">
-    <?php echo Form::open(array('method' => 'post', 'class' => 'login')); ?>
-    <section class="title">
-        <img class="login-img-logo" src="./app/templates/default/img/Pre-3-Logo.png" >
-        <h3 class="welcome">Property Management System</h3>
-    </section>
-    <div class="login-group">
-        <label for="email">Email</label>
-        <?php echo Form::input(array('name' => 'email', 'class' => 'form-control mb-1')); ?>
-        <label for="password">Password</label>
-        <?php echo Form::input(array('type' => 'password', 'name' => 'password', 'class' => 'form-control mb-1')); ?>
-        <a href="reset">Forgot Password?</a>
-        <?php echo Form::input(array('type' => 'submit', 'name' => 'submit', 'class' => 'btn btn-primary btn-block mt-1 btn-login', 'value' => 'Log In')); ?>
+
+    <div class="login-wrapper">
+        <?php echo Form::open(array('method' => 'post', 'class' => 'login')); ?>
+        <section class="title">
+            
+            <h3 class="welcome">Property Management System</h3>
+        </section>
+        <div class="login-group">
+            <label for="email">Email</label>
+            <?php echo Form::input(array('name' => 'email', 'class' => 'form-control mb-1')); ?>
+            <label for="password">Password</label>
+            <?php echo Form::input(array('type' => 'password', 'name' => 'password', 'class' => 'form-control mb-1')); ?>
+            <a href="reset">Forgot Password?</a>
+            <?php echo Form::input(array('type' => 'submit', 'name' => 'submit', 'class' => 'btn btn-primary btn-block mt-1 btn-login', 'value' => 'Log In')); ?>
+        </div>
+        <?php echo Form::close(); ?>
     </div>
-    <?php echo Form::close(); ?>
+  </div>
 </div>
